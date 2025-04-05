f73f15341436d14eb6d5d73682d2d655cde4222a
nikita <nikita.ratkevich91@mail.ru>
2025-04-05 15:45:42 +0200

    added ya metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Lato, Montserrat, Rubik } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 const lato = Lato({
   weight: ["400", "700"],
@@ -53,30 +54,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
     <Head>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-             (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-               m[i].l=1*new Date();
-               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-            
-               ym(100804725, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-               })
-            `,
-        }}
-      />
+
       <title>&quot;Mindbodyface | Medically proven methods and techniques to look younger&quot;</title>
     </Head>
     <body className={`${lato.variable} ${montserrat.variable} ${rubik.variable} antialiased`}>
     {children}
     </body>
+    <Script id="metrika-counter" strategy="afterInteractive">
+      {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+               m[i].l=1*new Date();
+               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+               (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            
+               ym(100786892, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+               })`
+      }
+    </Script>
     </html>
   );
 }
